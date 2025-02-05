070e55b8c13746112a9457a0d09feb0d784acfea
friedfox <finc8888@gmail.com>
2025-02-05 14:54:21 +0300

    Formating code with prettier

--- public/js/clarity.js
+++ public/js/clarity.js
@@ -1,20 +1,20 @@
-console.log('from clarity');
+console.log("from clarity");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const clarityTracking = document.querySelector('.js-clarity-tracking');
-    console.log('elem', clarityTracking);
-    setTimeout(() => {
-        console.log('window.clarity', window.clarity);
-        if (window.clarity && clarityTracking) {
-            window.addEventListener("consentGranted", () => {
-                console.log('consent granted');
-                window.clarity('consent');
-            });
-            const key = clarityTracking.dataset.key;
-            const value = clarityTracking.dataset.value;
-            console.log('key', key);
-            console.log('value', value);
-            window.clarity('set', key, value);
-        }
-    }, 1000);
+document.addEventListener("DOMContentLoaded", () => {
+  const clarityTracking = document.querySelector(".js-clarity-tracking");
+  console.log("elem", clarityTracking);
+  setTimeout(() => {
+    console.log("window.clarity", window.clarity);
+    if (window.clarity && clarityTracking) {
+      window.addEventListener("consentGranted", () => {
+        console.log("consent granted");
+        window.clarity("consent");
+      });
+      const key = clarityTracking.dataset.key;
+      const value = clarityTracking.dataset.value;
+      console.log("key", key);
+      console.log("value", value);
+      window.clarity("set", key, value);
+    }
+  }, 1000);
 });
